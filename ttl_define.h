793f53421be6a61d9a13b49f82c22cc5c7ce1fe3
TEM <tem-00@childs.squares.net>
2022-02-13 20:32:25 +0900

    デバッグ用関数 PCS を char* でも std::string でも呼び出せるように。

--- ttl_define.h
+++ ttl_define.h
@@ -23,20 +23,26 @@
 #define FILE_LINE     __FILE__ "(" TO_STR_2( __LINE__ ) ")"
 
 #ifdef DEBUG
-#  include <stdio.h>
-#  include <string>
+#include <stdio.h>
+#include <string>
 #  define PCD()            (printf( "%s(%d) : gone.\n", __FILE__, __LINE__ ), fflush( stdout ))
 #  define PCA( type, obj ) (printf( "%s(%d) : %s = " type "\n", __FILE__, __LINE__, #obj, (obj) ), fflush( stdout ))
 #  define PCC( c )         PCA( "%c", c )
 #  define PCI( i )         PCA( "%d", i )
 #  define PCX( x )         PCA( "0x%X", x )
 #  define PCP( p )         PCA( "%p", p )
-#  define PCS( s )         PCA( "%s", s )
+#  define PCS( s )         PCS_BODY( s, #s, __FILE__, __LINE__ )
 #  define PCF( f )         PCA( "%f", f )
 #  define PCLL( l )        PCA( "%lld", l )
 #  define PCLF( d )        PCA( "%lf", d )
 #  define PCSS( s )        (printf( "%s(%d) : %s = %s\n", __FILE__, __LINE__, #s, (s).c_str() ), fflush( stdout ))
 #  define PCB( b )         (printf( "%s(%d) : %s = %s\n", __FILE__, __LINE__, #b, (b) ? "true" : "false" ), fflush( stdout ))
+inline void PCS_BODY( const char* s, const char* name, const char* f, unsigned int l ) {
+  printf( "%s(%d) : %s = %s\n", f, l, name, s );
+  fflush( stdout );
+}
+inline void PCS_BODY( const std::string& s, const char* name, const char* f, unsigned int l  ) { PCS_BODY( str.c_str(), name, f, l ); }
+
 
 #  include "tt_utility.h"
 #  define PMD()            TtUtility::MessageBoxForDebug( "", "gone", 0, __FILE__, __LINE__ )
